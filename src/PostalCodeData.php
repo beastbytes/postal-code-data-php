6267e21b4aa25c631d39b77bf82004c2566654e2
Chris Yates <chris@clyates.plus.com>
2022-09-18 17:47:34 +0100

    Change of variable name

--- src/PostalCodeData.php
+++ src/PostalCodeData.php
@@ -13,28 +13,28 @@ use InvalidArgumentException;
 
 final class PostalCodeData implements PostalCodeDataInterface
 {
-    public function __construct(private array|string|null $postalCodes = null)
+    public function __construct(private array|string|null $postalCodeData = null)
     {
-        if ($this->postalCodes === null) {
-            $this->postalCodes = require 'postalCodes.php';
-        } elseif (is_string($this->postalCodes)) {
-            $this->postalCodes = require $this->postalCodes;
+        if ($this->postalCodeData === null) {
+            $this->postalCodeData = require 'postalCodes.php';
+        } elseif (is_string($this->postalCodeData)) {
+            $this->postalCodeData = require $this->postalCodeData;
         }
 
-        if (!is_array($this->postalCodes)) {
-            throw new InvalidArgumentException('`$postalCodes` must be an array of Postal Code formats, a path to a file that returns an array of Postal Code formats, or `null` to use local data');
+        if (!is_array($this->postalCodeData)) {
+            throw new InvalidArgumentException('`$postalCodeData` must be an array of Postal Code data, a path to a file that returns an array of Postal Code data, or `null` to use local data');
         }
     }
 
     public function getCountries(): array
     {
-        return array_keys($this->postalCodes);
+        return array_keys($this->postalCodeData);
     }
 
     public function getPattern(string $country): string
     {
         if ($this->hasCountry($country)) {
-            return $this->postalCodes[$country];
+            return $this->postalCodeData[$country];
         }
 
         throw new InvalidArgumentException(strtr(
@@ -45,6 +45,6 @@ final class PostalCodeData implements PostalCodeDataInterface
 
     public function hasCountry(string $country): bool
     {
-        return array_key_exists($country, $this->postalCodes);
+        return array_key_exists($country, $this->postalCodeData);
     }
 }
