b7b7b6a2fead3d459196ceabefb4c869e81b5963
Chris Yates <beastbytes@users.noreply.github.com>
2024-10-03 19:36:09 +0100

    Update data.php

--- data/data.php
+++ data/data.php
@@ -1,1 +1,406 @@
+<?php
+/**
+ * @copyright Copyright © 2024 BeastBytes - All rights reserved
+ * @license BSD 3-Clause
+ */
 
+declare(strict_types=1);
+
+return [
+    'AD' => [ // Andorra
+        'country' => 'AD',
+        'postalCode' =>  'AD200',
+    ],
+    'AL' => [ // Albania
+        'country' => 'AL',
+        'postalCode' =>  '5372',
+    ],
+    'AM' => [ // Armenia
+        'country' => 'AM',
+        'postalCode' =>  '5372',
+    ],
+    'AR' => [ // Argentina
+        'country' => 'AR',
+        'postalCode' =>  'A4819DER',
+    ],
+    'AT' => [ // Austria
+        'country' => 'AT',
+        'postalCode' =>  '5372',
+    ],
+    'AU' => [ // Australia
+        'country' => 'AU',
+        'postalCode' =>  '5372',
+    ],
+    'AZ' => [
+        'country' => 'AZ',
+        'postalCode' =>  'AZ9127', // Azerbaijan
+    ],
+    'BB' => [
+        'country' => 'BB',
+        'postalCode' =>  'BB42630', // Barbados
+    ],
+    'BE' => [
+        'country' => 'BE',
+        'postalCode' =>  '6423', // Belgium
+    ],
+    'BG' => [
+        'country' => 'BG',
+        'postalCode' =>  '4692', // Bulgaria
+    ],
+    'BN' => [
+        'country' => 'BN',
+        'postalCode' =>  'KJ9127', // Brunei
+    ],
+    'BR' => [
+        'country' => 'BR',
+        'postalCode' =>  '58234-439', // Brazil
+    ],
+    'CA' => [
+        'country' => 'CA',
+        'postalCode' =>  'A2Z 0M9', // Canada
+    ],
+    'CH' => [
+        'country' => 'CH',
+        'postalCode' =>  '7733', // Switzerland
+    ],
+    'CL' => [
+        'country' => 'CL',
+        'postalCode' =>  '827-9127', // Chile
+    ],
+    'CO' => [
+        'country' => 'CO',
+        'postalCode' =>  '432741', // Colombia
+    ],
+    'CR' => [
+        'country' => 'CR',
+        'postalCode' =>  '56283-5342', // Costa Rica
+    ],
+    'CY' => [
+        'country' => 'CY',
+        'postalCode' =>  '99654', // Cyprus
+    ],
+    'CZ' => [
+        'country' => 'CZ',
+        'postalCode' =>  '627 27', // Czech Republic
+    ],
+    'DE' => [
+        'country' => 'DE',
+        'postalCode' =>  '42630', // Germany
+    ],
+    'DK' => [
+        'country' => 'DK',
+        'postalCode' =>  '2549', // Denmark
+    ],
+    'DO' => [
+        'country' => 'DO',
+        'postalCode' =>  '42630', // Dominican Republic
+    ],
+    'DZ' => [
+        'country' => 'DZ',
+        'postalCode' =>  '42630', // Algeria
+    ],
+    'EC' => [
+        'country' => 'EC',
+        'postalCode' =>  '427401', // Ecuador
+    ],
+    'EE' => [
+        'country' => 'EE',
+        'postalCode' =>  '42630', // Estonia
+    ],
+    'EG' => [
+        'country' => 'EG',
+        'postalCode' =>  '42630', // Egypt
+    ],
+    'ES' => [
+        'country' => 'ES',
+        'postalCode' =>  '42630', // Spain
+    ],
+    'FI' => [
+        'country' => 'FI',
+        'postalCode' =>  '42630', // Finland
+    ],
+    'FK' => [
+        'country' => 'FK',
+        'postalCode' =>  'FIQQ 1ZZ', // Falkland Islands
+    ],
+    'FO' => [
+        'country' => 'FO',
+        'postalCode' =>  'FO-543', // Faroe Islands
+    ],
+    'FR' => [
+        'country' => 'FR',
+        'postalCode' =>  '42630', // France
+    ],
+    'GB' => [
+        'country' => 'GB',
+        'postalCode' =>  'SW1 1AA',
+    ],
+    'GG' => [
+        'country' => 'GG',
+        'postalCode' =>  'GY79 4TH', // Guernsey (GB postal code format)
+    ],
+    'GH' => [
+        'country' => 'GH',
+        'postalCode' =>  'AZ13246', // Ghana
+    ],
+    'GI' => [
+        'country' => 'GI',
+        'postalCode' =>  'GX11 1AA', // Gibraltar
+    ],
+    'GR' => [
+        'country' => 'GR',
+        'postalCode' =>  '27 27', // Greece
+    ],
+    'GS' => [
+        'country' => 'GS',
+        'postalCode' =>  'SIQQ 1ZZ', // South Georgia and the South Sandwich Islands
+    ],
+    'GT' => [
+        'country' => 'GT',
+        'postalCode' =>  '42630', // Guatemala
+    ],
+    'HU' => [
+        'country' => 'HU',
+        'postalCode' =>  '2549', // Hungary
+    ],
+    'ID' => [
+        'country' => 'ID',
+        'postalCode' =>  '69127', // Indonesia
+    ],
+    'IE' => [
+        'country' => 'IE',
+        'postalCode' =>  'F75 FEK0', // Ireland
+    ],
+    'IL' => [
+        'country' => 'IL',
+        'postalCode' =>  '7235106', // Israel
+    ],
+    'IM' => [
+        'country' => 'IM',
+        'postalCode' =>  'IM32 3LW', // Isle of Man (GB postal code format)
+    ],
+    'IN' => [
+        'country' => 'IN',
+        'postalCode' =>  '431 764', // India
+    ],
+    'IO' => [
+        'country' => 'IO',
+        'postalCode' =>  'BBND 1ZZ', // British Indian Ocean Territory
+    ],
+    'IQ' => [
+        'country' => 'IQ',
+        'postalCode' =>  '34534', // Iraq
+    ],
+    'IT' => [
+        'country' => 'IT',
+        'postalCode' =>  '53928', // Italy
+    ],
+    'JE' => [
+        'country' => 'JE',
+        'postalCode' =>  'JE8 4LW', // Jersey (GB postal code format)
+    ],
+    'JP' => [
+        'country' => 'JP',
+        'postalCode' =>  '543-9127', // Japan
+    ],
+    'KR' => [
+        'country' => 'KR',
+        'postalCode' =>  '42630', // South Korea
+    ],
+    'LK' => [
+        'country' => 'LK',
+        'postalCode' =>  '42630', // Sri Lanka
+    ],
+    'LT' => [
+        'country' => 'LT',
+        'postalCode' =>  'LT-42630', // Lithuania
+    ],
+    'LU' => [
+        'country' => 'LU',
+        'postalCode' =>  '99127', // Luxembourg
+    ],
+    'LV' => [
+        'country' => 'LV',
+        'postalCode' =>  'LV-9127', // Latvia
+    ],
+    'MD' => [
+        'country' => 'MD',
+        'postalCode' =>  'MD-9127', // Moldova
+    ],
+    'ME' => [
+        'country' => 'ME',
+        'postalCode' =>  '42630', // Montenegro
+    ],
+    'MK' => [
+        'country' => 'MK',
+        'postalCode' =>  '5439', // Macedonia
+    ],
+    'MM' => [
+        'country' => 'MM',
+        'postalCode' =>  '42630', // Myanmar
+    ],
+    'MS' => [
+        'country' => 'MS',
+        'postalCode' =>  'MSR1230', // Montserrat
+    ],
+    'MT' => [
+        'country' => 'MT',
+        'postalCode' =>  'ADX 9127', // Malta
+    ],
+    'MU' => [
+        'country' => 'MU',
+        'postalCode' =>  'A9127', // Mauritius
+    ],
+    'MX' => [
+        'country' => 'MX',
+        'postalCode' =>  '42630', // Mexico
+    ],
+    'MY' => [
+        'country' => 'MY',
+        'postalCode' =>  '42630', // Malaysia
+    ],
+    'NG' => [
+        'country' => 'NG',
+        'postalCode' =>  '432401', // Nigeria
+    ],
+    'NI' => [
+        'country' => 'NI',
+        'postalCode' =>  '59127', // Nicaragua
+    ],
+    'NL' => [
+        'country' => 'NL',
+        'postalCode' =>  '639 NL', // Netherlands
+    ],
+    'NO' => [
+        'country' => 'NO',
+        'postalCode' =>  '5372', // Norway
+    ],
+    'NZ' => [
+        'country' => 'NZ',
+        'postalCode' =>  '5372', // New Zealand
+    ],
+    'PA' => [
+        'country' => 'PA',
+        'postalCode' =>  '0743', // Panama
+    ],
+    'PE' => [
+        'country' => 'PE',
+        'postalCode' =>  '42630', // Peru
+    ],
+    'PH' => [
+        'country' => 'PH',
+        'postalCode' =>  '5372', // Philippines
+    ],
+    'PK' => [
+        'country' => 'PK',
+        'postalCode' =>  '42630', // Pakistan
+    ],
+    'PL' => [
+        'country' => 'PL',
+        'postalCode' =>  '27-634', // Poland
+    ],
+    'PN' => [
+        'country' => 'PN',
+        'postalCode' =>  'PCRN 1ZZ', // Pitcairn Islands
+    ],
+    'PR' => [
+        'country' => 'PR',
+        'postalCode' =>  '00727-9876', // Puerto Rico
+    ],
+    'PT' => [
+        'country' => 'PT',
+        'postalCode' =>  '5825-380', // Portugal
+    ],
+    'PY' => [
+        'country' => 'PY',
+        'postalCode' =>  '5372', // Paraguay
+    ],
+    'RO' => [
+        'country' => 'RO',
+        'postalCode' =>  '437401', // Romania
+    ],
+    'RS' => [
+        'country' => 'RS',
+        'postalCode' =>  '23917', // Serbia
+    ],
+    'RU' => [
+        'country' => 'RU',
+        'postalCode' =>  '427401', // Russia
+    ],
+    'SA' => [
+        'country' => 'SA',
+        'postalCode' =>  '47193-9127', // Saudi Arabia
+    ],
+    'SE' => [
+        'country' => 'SE',
+        'postalCode' =>  'SE-543-27', // Sweden
+    ],
+    'SG' => [
+        'country' => 'SG',
+        'postalCode' =>  '432740', // Singapore
+    ],
+    'SH' => [
+        'country' => 'SH',
+        'postalCode' =>  'STHL 1ZZ', // Saint Helena, Ascension and Tristan da Cunha
+    ],
+    'SI' => [
+        'country' => 'SI',
+        'postalCode' =>  '5372', // Slovenia
+    ],
+    'SK' => [
+        'country' => 'SK',
+        'postalCode' =>  '827 27', // Slovakia
+    ],
+    'TC' => [
+        'country' => 'TC',
+        'postalCode' =>  'TKCA 1ZZ', // Turks and Caicos Islands
+    ],
+    'TH' => [
+        'country' => 'TH',
+        'postalCode' =>  '79127', // Thailand
+    ],
+    'TN' => [
+        'country' => 'TN',
+        'postalCode' =>  '6742', // Tunisia
+    ],
+    'TR' => [
+        'country' => 'TR',
+        'postalCode' =>  '42630', // Turkey
+    ],
+    'TW' => [
+        'country' => 'TW',
+        'postalCode' =>  '427850', // Taiwan
+    ],
+    'UA' => [
+        'country' => 'UA',
+        'postalCode' =>  '42630', // Ukraine
+    ],
+    'US' => [
+        'country' => 'US',
+        'postalCode' =>  '48139', // United States of America
+    ],
+    'UY' => [
+        'country' => 'UY',
+        'postalCode' =>  '42630', // Uruguay
+    ],
+    'VE' => [
+        'country' => 'VE',
+        'postalCode' =>  '4364-A', // Venezuela
+    ],
+    'VI' => [
+        'country' => 'VI',
+        'postalCode' =>  '00827 5492', // United States Virgin Islands
+    ],
+    'VN' => [
+        'country' => 'VN',
+        'postalCode' =>  '42630', // Vietnam
+    ],
+    'XK' => [
+        'country' => 'XK',
+        'postalCode' =>  '40027', // Kosovo NOTE: XK is a temporary code used until ISO assign a permanent code
+    ],
+    'ZA' => [
+        'country' => 'ZA',
+        'postalCode' =>  '5372', // South Africa
+    ],
+];
